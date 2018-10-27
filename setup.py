1424d1622894d6c3abaf06dd6f235c43dd7bb4dd
enochtangg <enochtangg@gmail.com>
2018-10-27 17:49:27 -0400

    initialize slam project

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='TrafficSignRecognition',
+    version='0.1.0',
+    author='Enoch Tang',
+    author_email='enoch.tang@hotmail.com',
+    packages=['slam'],
+    scripts=[],
+    url='',
+    license='LICENSE.txt',
+    description='',
+    long_description=open('README.md').read(),
+    install_requires=[
+    ],
+)
