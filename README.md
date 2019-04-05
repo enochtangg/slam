ced94a4af59e35c8690612c12cfed8b40af67bc2
enochtangg <enoch.tang@hotmail.com>
2019-04-05 14:11:14 -0400

    bf matching

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 * look into what does matching frames do (BFMatcher, KnnMatch)
 * implement match_frames
 * use triangulate to get points in 3 dimensional space
+* 4:08:49
 
 ## DONE:
 * seup python package environment
--- src/frame.py
+++ src/frame.py
@@ -23,14 +23,42 @@ class Frame:
             self.kpus, self.des = self.feature_extractor.extract(img)
             self.pts = [None]*len(self.kpus)
 
-    # def match_frames(self, kps, des):
-    #     # Matching
-    #     matches = None
-    #     if self.last is not None:
-    #         matches = cv2.BFMatcher().match(des, self.last['des'])
-    #     self.last = {'kps': kps, 'des': des}
-    #
-    #     return matches
+    def match_frames(self, f1, f2):
+        # Matching previous frame and current frame (used in triangulation
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+
+        try:
+            matches = bf.knnMatch(f1.des, f2.des, k=2)
+        except cv2.error:
+            return False
+
+        good_matches = []
+        idx_of_des_from_f1, idx_of_des_from_f2 = [], []
+        idx_set1, idx_set2 = set(), set()
+
+        # using Lowe's ratio to filter out bad kps
+        for m, n in matches:
+            if m.distance < 0.75 * n.distance:
+                p1 = f1.kpus[m.queryIdx]
+                p2 = f2.kpus[m.trainIdx]
+
+                # ensure distance is within 32
+                if m.distance < 32:
+                    if m.queryIdx not in idx_set1 and m.trainIdx not in idx_set2:
+                        idx_of_des_from_f1.append(m.queryIdx)
+                        idx_of_des_from_f2.append(m.trainIdx)
+                        idx_set1.add(m.queryIdx)
+                        idx_set2.add(m.trainIdx)
+
+                        good_matches.append((p1, p2))
+
+        print(good_matches)
+
+        # now we are left with good matches
+        # we want see all the points are are inliers with ransac
+        # then return the index_ofde
+
+        return matches
 
     def process_frame(self, img):
         img = cv2.resize(img, (self.display.W, self.display.H))
@@ -42,7 +70,7 @@ class Frame:
             return
 
         # implement some matching frame function that takes the last two frames from self.frames
-        # pts, Rt = self.match_frames(self.frames[-1], self.frames[-2])
+        pts, Rt = self.match_frames(self.frames[-1], self.frames[-2])
         # cv2.triangulatePoints(IRt, Rt, pts[:,0].T, pts[:,1].T).T
 
         # for 2D display
